6638b70e82e13cefacbd5672acdf32380fb09aa2
Alfred Turkson <111619866+Codeghod@users.noreply.github.com>
2022-09-15 16:54:33 -0700

    Add files via upload

---
+++ str_var.py
@@ -0,0 +1,8 @@
+#assingning a variable to a string
+
+name = 'Alfred Turkson'
+age ='your age is 22'
+favourite_color = 'your favourite color is yellow'
+print(name)
+print(age)
+print(favourite_color)
